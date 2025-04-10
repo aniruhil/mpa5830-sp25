5c1da9bece2f5fb7645464fbdba83db796e266e1
ani ruhil <ruhil@ohio.edu>
2025-04-10 10:22:01 -0400

    Create install.R

---
+++ install.R
@@ -0,0 +1,17 @@
+install.packages("tidyverse")
+install.packages("rmarkdown")
+install.packages("httr")
+install.packages("shinydashboard")
+install.packages("leaflet")
+install.packages("remotes")
+remotes::install_github("walkerke/tigris")
+remotes::install_github("walkerke/tidycensus")
+install.packages("patchwork")
+remotes::install_github("UrbanInstitute/urbnmapr")
+install.packages("ggthemes")
+install.packages("plotly")
+install.packages("highcharter")
+
+
+
+
